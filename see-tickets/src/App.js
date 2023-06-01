b7b4a9d515a4e4cc88e9e8280d7d43f5f8e654c9
ismail <ismail.bouasria@laplateforme.io>
2023-06-01 19:40:25 +0200

    update EventRates : add style and content

--- see-tickets/src/App.js
+++ see-tickets/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 
   return (
-    <div className=" bg-gray-400 w-screen h-screen flex">
+    <div className=" bg-gray-300 w-screen h-screen flex justify-center">
  
         
       <EventRatesScreen />
--- see-tickets/src/screen/EventRatesScreen.js
+++ see-tickets/src/screen/EventRatesScreen.js
@@ -1,11 +1,36 @@
+import { AiFillInfoCircle } from 'react-icons/ai';
 
 function EventRatesScreen() {
-    return (
-        <div>
-         <button className='bg-black  text-white'> Français </button>
-        </div>
-      
-    );
-  }
-  
+  return (
+    <div className="flex flex-col h-screen w-screen m-5">
+      <div className="flex justify-end">
+        <button className='bg-black text-white rounded-md text-center font-bold h-9 w-28'> Français ▾ </button>
+      </div>
+      <div className="flex items-center mt-5">
+        <section className="relative text-white">
+          <img className="rounded-lg" src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" />
+          <div className='absolute font inset-0 flex justify-end m-1'>
+            <AiFillInfoCircle size={24} />
+          </div>
+          <p className="absolute inset-0 flex items-center justify-center"> <strong>Andrew Alfred</strong> </p>
+          <p className="absolute inset-0 flex items-center justify-center mt-11">Technical advisor</p>
+        </section>
+      </div>
+      <div className="flex justify-end mt-2">
+        <button className='text-white rounded-full text-center text-xs w-16 h-5 bg-teal-600'> + d'infos </button>
+      </div>
+      <div className='flex justify-start'>
+       <p className='rounded-full bg-teal-600 w-5 text-sm text-center text-white w-'> 1 </p> <strong> Places</strong>
+      </div>
+      <div className='rounded bg-slate-100 drop-shadow-lg mt-4 flex flex-col'>
+        <div className='m-2 flex justify-between text-zinc-500 font-bold'> 
+        <p> Ven. 16 juin 2023 </p> <span cl> 20h-4h</span> </div> 
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
 export default EventRatesScreen;
