181b7f341be565641222f75a8e4baab09d7e64e3
ismail <ismail.bouasria@laplateforme.io>
2023-06-01 15:19:23 +0200

    create EventRatesScreen

--- see-tickets/src/App.js
+++ see-tickets/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import EventRatesScreen from './screen/EventRatesScreen';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-gray-400 w-screen h-screen flex">
+ 
+        
+      <EventRatesScreen />
+
     </div>
   );
 }
