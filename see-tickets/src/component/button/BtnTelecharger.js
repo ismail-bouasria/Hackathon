686e42729939c0f8a99e6c8fd443fe5c4d55eb3d
jeremy <jeremy.nowak@laplateforme.io>
2023-06-01 19:36:53 +0200

    create: /validationPage/linkTicket

---
+++ see-tickets/src/component/button/BtnTelecharger.js
@@ -0,0 +1,19 @@
+import billetExemple from "../../asset/billetExemple.pdf"
+
+
+function BtnTelecharger () {
+  return (
+    
+
+    <a
+        href={billetExemple}
+        download="Example-PDF-document"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button className="w-80 bg-pink-600 rounded-md " >Télécharger</button>
+  </a>
+  );
+}
+
+  export default BtnTelecharger;
